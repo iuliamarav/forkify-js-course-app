09cbaee79376c3e91d808f71b54ead333e53c7d0
iuliamarav <iuliamara.valean@gmail.com>
2022-03-01 12:53:45 +0200

    Modified npm Fractional to fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,7 @@ import View from './view.js';
 
 // import icons from '../img/icons.svg' //parcel 1
 import icons from 'url:../../img/icons.svg'; //parcel2
-import { Fraction } from 'fractional';
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -128,7 +128,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
+        ing.quantity ? fracty(ing.quantity).toString() : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
